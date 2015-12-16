327b2544218b4546a5c668a9610052ef1a13bbc4
Chris Thoburn <runspired@gmail.com>
2015-12-15 18:56:41 -0600

    adds ember updating toolbelt

---
+++ blueprints/toolbelt-update-ember/index.js
@@ -0,0 +1,70 @@
+/*jshint node:true*/
+var installToolbelt = require('../../lib/install-toolbelt');
+
+module.exports = {
+  description: 'Bump Ember dependencies to their latest versions.',
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function(options) {
+    return installToolbelt.bind(this)([
+      {
+        name: 'Ember',
+        description: "Bump Ember",
+        bowerPackages: [
+          'ember',
+          'ember-resolver',
+          'ember-cli-test-loader',
+          'ember-cli-shims',
+          'ember-cli/loader.js',
+          'ember-load-initializers'
+        ]
+      },
+      {
+        name: 'Ember CLI',
+        description: "Bump Ember-Cli",
+        packages: [
+          'ember-cli',
+          //'ember-cli-toolbelts',
+          'broccoli-asset-rev',
+          'ember-cli-app-version',
+          'ember-cli-content-security-policy',
+          'ember-cli-dependency-checker',
+          'ember-cli-htmlbars',
+          'ember-cli-inject-live-reload',
+          'ember-cli-release',
+          'ember-cli-sri',
+          'ember-cli-uglify',
+          'ember-disable-proxy-controllers',
+          'ember-export-application-global',
+          'ember-disable-prototype-extensions',
+          'ember-try'
+          //ember-cli-babel
+        ],
+        bowerPackages: ['ember', 'ember-data']
+      },
+      {
+        name: 'Ember Data',
+        description: "Bump Ember-Data",
+        packages: [
+          'ember-data'
+        ],
+        bowerPackages: [
+          'ember-data'
+        ]
+      },
+      {
+        name: 'Ember Qunit',
+        description: "Bump Ember-Qunit",
+        packages: [
+          'ember-cli-qunit'
+        ],
+        bowerPackages: [
+          'qunit',
+          'ember-qunit',
+          'ember-qunit-notifications'
+        ]
+      }
+    ]);
+  }
+};
