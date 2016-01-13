70e15eae3daccaa47fea2b780e10f0bfbd1e97be
Chris Thoburn <runspired@gmail.com>
2016-01-13 12:06:35 -0600

    fix(mobile): adds hammertime option and improves descriptions

--- blueprints/toolbelt-mobile/index.js
+++ blueprints/toolbelt-mobile/index.js
@@ -4,17 +4,21 @@ var toolbelt = require('../../lib/toolbelt');
 module.exports = toolbelt({
   description: 'Installs Addons For Mobile Support',
 
-  afterInstall: function(options) {
+  afterInstall: function() {
     return this.installToolbelt([
       {
-        name: 'Touch Input',
-        description: "nuh, nuh, nuh, nuh",
+        name: 'Remove Click/Touch Delay',
+        description: "ember-hammertime seamlessly wires the hammer-time touch-action polyfill into your app." +
+         "hammer-time is a better fastclick through polyfill.",
+        addons: [
+          'ember-hammertime'
+        ]
+      },
+      {
+        name: 'Incorporate Gestures such as Pan/Swipe',
+        description: 'ember-gestures provides an easy way to add HammerJS to your app.',
         addons: [
           'ember-gestures'
-        ],
-        bowerPackages: [
-          'hammer-time',
-          'hammerjs'
         ]
       }
     ]);
