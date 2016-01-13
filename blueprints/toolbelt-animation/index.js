629151021e7483db887cf242cba1529f17082690
Chris Thoburn <runspired@gmail.com>
2016-01-13 12:02:38 -0600

    feat(toolbelt-animation): adds an animation toolbelt

---
+++ blueprints/toolbelt-animation/index.js
@@ -0,0 +1,29 @@
+/*jshint node:true*/
+"use strict";
+
+var toolbelt = require('../../lib/toolbelt');
+
+module.exports = toolbelt({
+  description: 'Installs addons for animation and transitions.',
+
+  afterInstall: function(options) {
+    this.installToolbelt([
+      {
+        name: 'Liquid-Fire',
+        description: "Installs liquid-fire and recommended liquid-fire addons",
+        addons: [
+          'liquid-fire',
+          'liquid-fire-tweenlite',
+          'liquid-fire-hooks'
+        ]
+      },
+      {
+        name: 'Liquid-Tether',
+        description: "Installs liquid-tether for animating modals, dropdowns, and popovers",
+        addons: [
+          'liquid-tether'
+        ]
+      }
+    ]);
+  }
+});
